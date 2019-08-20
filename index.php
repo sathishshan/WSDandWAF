b811e47ec7ff2f64facdbb1ce6fffe0bbc314dd7
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-08-20 20:11:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html><head><title>EXPLOIT - Linux</title></head><body>
+<pre>
+
+                 _       _ _   
+                | |     (_) |  
+  _____  ___ __ | | ___  _| |_ 
+ / _ \ \/ / '_ \| |/ _ \| | __|
+|  __/>  <| |_) | | (_) | | |_ 
+ \___/_/\_\ .__/|_|\___/|_|\__|
+          | |                  
+          |_|                  
+
+
+
+</pre>
+<ul>
+<li><a href="/wordpress.com/">WordPress</a></li>
+<li><a href="/joomla.com/">Joomla</a></li>
+<li><a href="/cors.com/">Cors-Exploitation</a></li>
+<li><a href="/phpmyadmin/">phpMyAdmin</a></li>
+</ul>
+</body>
+</html>
+
